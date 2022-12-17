48cc3f8fb140f40376c7c221387ccbd9f4d33816
Mavin Sandeep <62639000+userdevil@users.noreply.github.com>
2022-12-17 23:38:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -55,4 +55,4 @@ def site_map():
   return render_template("sitemape.xml")
 
 if __name__=='__main__':
-    app.run(debug=True,host='0.0.0.0',port=int(os.environ.get('PORT', 8080)))
+    app.run(debug=True)
